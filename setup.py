59195345921125fd222d12d01cfd5ff98f60d2e3
Ruzsan Bhadha <ruzanbhadha@gmail.com>
2025-03-12 14:03:20 +0530

    Project structure ready, ven set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ruzsan",
+    author_email="ruzsanbhadha@gmail.com",
+    packages=find_packages()
+)
